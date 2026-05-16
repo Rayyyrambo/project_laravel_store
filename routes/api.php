9ce0c4b8e090d2f66ef75c8433d4b531519b1c63
Muh Rayyan Maulana <rayymaull@gmail.com>
2026-05-17 01:46:30 +0800

    menambahkan Api pada  massage

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,14 @@
 <?php
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\MassageController ;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
-
+Route::get('/massage',[MassageController::class, 'index' ]);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
